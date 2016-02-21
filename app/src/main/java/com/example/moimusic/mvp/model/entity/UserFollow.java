2da6d63e550ddcda2ab55d56f205b8ad8ec195f1
fallllllll <348476129@qq.com>
2016-02-21 17:24:21 +0800

    完成了个人中心的制作

---
+++ app/src/main/java/com/example/moimusic/mvp/model/entity/UserFollow.java
@@ -0,0 +1,34 @@
+package com.example.moimusic.mvp.model.entity;
+
+import cn.bmob.v3.BmobObject;
+
+/**
+ * Created by qqq34 on 2016/2/21.
+ */
+public class UserFollow extends BmobObject {
+    private String userId,followId;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFollowId() {
+        return followId;
+    }
+
+    public void setFollowId(String followId) {
+        this.followId = followId;
+    }
+
+    @Override
+    public String toString() {
+        return "UserFollow{" +
+                "userId='" + userId + '\'' +
+                ", followId='" + followId + '\'' +
+                '}';
+    }
+}
