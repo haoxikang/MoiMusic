a8b4a9fdd750f0ed1912e063a00094b49f5d0028
sgsd63584 <348476129@qq.com>
2016-03-24 16:09:47 +0800

    动态模块界面

---
+++ app/src/main/java/com/example/moimusic/mvp/model/entity/Trends.java
@@ -0,0 +1,72 @@
+package com.example.moimusic.mvp.model.entity;
+
+import cn.bmob.v3.BmobObject;
+import cn.bmob.v3.datatype.BmobFile;
+
+/**
+ * Created by Administrator on 2016/3/24.
+ */
+public class Trends extends BmobObject {
+    private MoiUser userid;
+    private Music songid;
+    private MusicList listid;
+    private String content,type;
+    private Integer likes;
+private BmobFile image;
+
+    public MoiUser getUserid() {
+        return userid;
+    }
+
+    public void setUserid(MoiUser userid) {
+        this.userid = userid;
+    }
+
+    public BmobFile getImage() {
+        return image;
+    }
+
+    public void setImage(BmobFile image) {
+        this.image = image;
+    }
+
+    public Integer getLikes() {
+        return likes;
+    }
+
+    public void setLikes(Integer likes) {
+        this.likes = likes;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public MusicList getListid() {
+        return listid;
+    }
+
+    public void setListid(MusicList listid) {
+        this.listid = listid;
+    }
+
+    public Music getSongid() {
+        return songid;
+    }
+
+    public void setSongid(Music songid) {
+        this.songid = songid;
+    }
+}
--- app/src/main/java/com/example/moimusic/ui/fragment/FragmentTrends.java
+++ app/src/main/java/com/example/moimusic/ui/fragment/FragmentTrends.java
@@ -3,25 +3,84 @@ package com.example.moimusic.ui.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.moimusic.R;
+import com.melnykov.fab.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by qqq34 on 2016/1/30.
  */
 public class FragmentTrends extends Fragment {
+    private RecyclerView list;
+    private List<String> mDatas;
+    private HomeAdapter mAdapter;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-
+    protected void initData()
+    {
+        mDatas = new ArrayList<String>();
+        for (int i = 'A'; i < 'z'; i++)
+        {
+            mDatas.add("" + (char) i);
+        }
+    }
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_trends, container, false);
+        initData();
+        FloatingActionButton fab = (FloatingActionButton) v.findViewById(R.id.fab);
+        list = (RecyclerView)v.findViewById(R.id.trendsList);
+fab.attachToRecyclerView(list);
+        list .setLayoutManager(new LinearLayoutManager(getContext()));
+        list .setAdapter(mAdapter = new HomeAdapter());
         return v;
+    }
+    class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder>
+    {
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            MyViewHolder holder = new MyViewHolder(LayoutInflater.from(
+                    getContext()).inflate(R.layout.item_home, parent,
+                    false));
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position)
+        {
+            holder.tv.setText(mDatas.get(position));
+        }
+
+        @Override
+        public int getItemCount()
+        {
+            return mDatas.size();
+        }
+
+        class MyViewHolder extends RecyclerView.ViewHolder
+        {
+
+            TextView tv;
+
+            public MyViewHolder(View view)
+            {
+                super(view);
+                tv = (TextView) view.findViewById(R.id.id_num);
+            }
+        }
     }
 }
